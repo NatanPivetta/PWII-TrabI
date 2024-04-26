3ca22bf806cd7fcee4f2d4b13a883fd3d228d742
natanpivetta <natanpivetta@github.com>
2024-04-26 14:13:59 -0300

    Criação de serviço Rest Client de cadastro

---
+++ cadastro/src/test/java/org/acme/CadastroIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class CadastroIT extends CadastroTest {
+    // Execute the same tests but in packaged mode.
+}
