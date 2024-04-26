cfd9c9dfce4beb3c75049026c76c1b5f28fff9bf
natanpivetta <natanpivetta@github.com>
2024-04-26 17:57:02 -0300

    Servico de Cadastro e Interface Rest, ainda com erro

--- cadastro/src/main/java/org/acme/Cadastro.java
+++ cadastro/src/main/java/org/acme/Cadastro.java
@@ -1,16 +1,30 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.FormParam;
+import main.java.org.acme.IMessage;
+import main.java.org.acme.Message;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+
+
 
 @Path("/hello")
 public class Cadastro {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Inject
+    @RestClient
+    IMessage service;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message hello(@FormParam("cpf") String cpf) {
+        return service.confirm(cpf);
     }
 }
---
+++ cadastro/src/main/java/org/acme/IMessage.java
@@ -0,0 +1,28 @@
+package main.java.org.acme;
+
+import java.beans.ConstructorProperties;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@RegisterRestClient(baseUri = "https://localhost:8080/")
+public interface IMessage {
+    
+
+
+
+    @POST
+    @Path("/cpfverify")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    IMessage confirm(
+        @FormParam("cpf") String cpf);
+}
